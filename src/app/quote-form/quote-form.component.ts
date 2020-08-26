fbe65357b3c57af70d3abd7d6cbc42e1bbf9698a
becky nayere <rebecca.nayere@gmail.com>
2020-08-26 20:31:54 +0300

    add styling to form component

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,18 +1,4 @@
-// import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-quote-form',
-//   templateUrl: './quote-form.component.html',
-//   styleUrls: ['./quote-form.component.css']
-// })
-// export class QuoteFormComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
