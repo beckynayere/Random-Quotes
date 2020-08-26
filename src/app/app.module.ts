261efc05f982a7ac5bb9d5fdca68d37379ab3863
becky nayere <rebecca.nayere@gmail.com>
2020-08-26 20:03:32 +0300

    add a directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { HighlightDirective } from './highlight.directive';
-import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateCountPipe } from './date-count.pipe';
+import { HighlightDirective } from './highlight.directive'
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    HighlightDirective,
     DateCountPipe,
+    HighlightDirective,
     QuoteFormComponent
   ],
   imports: [
@@ -26,3 +30,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,12 +1,10 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem: ElementRef) {
-    this.elem.nativeElement.style.backgroundColor = 'yellow';
-  }
+  constructor() { }
 
 }
